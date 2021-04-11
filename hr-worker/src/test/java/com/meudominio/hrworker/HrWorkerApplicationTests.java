d9a35548771a7ff762a8ffe18efd7e0da4bf231a
LuisPaulo1 <csluispaulo@gmail.com>
2021-04-11 10:24:22 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/meudominio/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
