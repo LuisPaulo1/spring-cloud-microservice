5a80969192ab9bebd9c30ed41cd82140c06db671
LuisPaulo1 <csluispaulo@gmail.com>
2021-04-13 22:39:44 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/meudominio/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meudominio.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
